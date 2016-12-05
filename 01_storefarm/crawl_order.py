0a35f95b8409909fc557ff8b223bf24235e9d27c
wooes23 <wooes23@gmail.com>
2016-12-05 11:01:42 +0900

    auto login

--- 01_storefarm/crawl_order.py
+++ 01_storefarm/crawl_order.py
@@ -1,4 +1,4 @@
-
+# -*- coding:utf8 -*-
 """
 1.crawl_data(selenium 활용)
     1. storefarm에 접속하고,
@@ -7,3 +7,39 @@
         a. 다운로드 경로 지정
     4. 다운로드 완료하고 주문내역 선택한 뒤 발주 확인
 """
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+userID = raw_input("ID : ")
+userPW = raw_input("password : ")
+
+# chromedriver를 이용하여 storefarm Login 창에 접속합니다.
+path = './chromedriver'
+driver = webdriver.Chrome(path)
+driver.get('https://sell.storefarm.naver.com/l/login/')
+
+
+# Login
+# ID와 PW 영역을 찾아줍니다
+el_name = driver.find_element_by_name('username')
+el_pw = driver.find_element_by_name('password')
+
+# userID 입력
+el_name.clear()
+el_name.send_keys(userID)
+
+# password 입력
+el_pw.clear()
+el_pw.send_keys(userPW)
+el_pw.send_keys(Keys.RETURN)
+
+
+#신규 주문내역이 있는지 확인
+
+
+# 주문내역이 있을 경우 주문내역 다운로드
+# 이떄 다운로드 경로는 바탕화면
+
+
+# 다운로드 완료 후 주문내역을 선택한 뒤 발주 확인
